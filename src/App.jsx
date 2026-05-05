041b86dbd5e3a0309cd8072e8ce5bdaa29d771d0
Nikolvillla <nikolvilla808@gmail.com>
2026-05-05 02:29:22 -0500

    Proyecto inicial creado con Vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// App.jsx - mi primer componenete de React
+import './App.css'
+
+function App() {
+  //TODO prendiz: cambia este titulo por el nombre de tu proyecto
+  const GestorTareas = "Gestor de Tareas  "
+
+  return(
+    <div className="App">
+      <h1>{GestorTareas}</h1>
+    <p>Si ves este texto react esta funcionando correctamente</p>  
+    </div>
+  )
+}
+export default App
