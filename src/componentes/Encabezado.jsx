0406f9659ea158beb354b7c27a1a2fb99fa10ff3
Nikolvillla <nikolvilla808@gmail.com>
2026-05-05 02:38:57 -0500

    Subiendo Encabezado.jsx

---
+++ src/componentes/Encabezado.jsx
@@ -0,0 +1,11 @@
+// Encabezado.jsx - Componente de encabezado para la aplicación
+function Encabezado() {
+    return (
+        <header>
+            <h1>Mis Tareas</h1>
+            <p>Organiza lo que tienes que hacer</p>
+        </header>
+    );
+}
+
+export default Encabezado;
